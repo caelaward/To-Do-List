5fcc114463b1a62e000df37f6b2033d4d01901a7
caelaward <146817394+caelaward@users.noreply.github.com>
2023-11-29 16:28:03 +0200

    added changes

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let btn2=document.querySelector('[data-btn2]')
+let input=document.querySelector('[data-input]')
+
+let list=[]
+function AddInput() {
+     
+}
+
+// function addInput() {
+//     localStorage.set("storage",JSON.stringify(array))
+//     let task=localStorage.getItem("storage")
+//     if (task==null) {
+//         array=[]
+//     }else{
+//         array=JSON.parse(task)
+//     }
+//     let item=document.getElementsByClassName("items")
+//     let table= array.map((items,index)=>{
+//        return `<tr>
+//         <td>${items.id}</td>
+//         <td>${items.name}</td>
+//         <td>${items.createdDate }</td>
+//         <td>${items.completed}</td>
+//        </tr>`
+//     })   
+//     }
+
+
+
