3d0ac80cdd2b0e6b0a656c6fbed500d501bc4788
caelaward <146817394+caelaward@users.noreply.github.com>
2023-12-01 12:52:49 +0200

    added js function

--- js/index.js
+++ js/index.js
@@ -1,29 +1,69 @@
 let btn2=document.querySelector('[data-btn2]')
 let input=document.querySelector('[data-input]')
-
+b  =1 
+c = new Date();
+d = false
 let list=[]
-function AddInput() {
-     
+
+btn2.addEventListener('click', function () {
+    let maxId = 0;
+
+
+    const newItem = AddInput(input.value, maxId + 1, new Date(), false);
+    list.push(newItem);
+    updateList();
+    addList()
+})
+
+
+
+function AddInput(name, id, date, completed) {
+    return {
+        name: name,
+        id: id,
+        createdDate: date,
+        completed: completed
+    };
 }
 
-// function addInput() {
-//     localStorage.set("storage",JSON.stringify(array))
-//     let task=localStorage.getItem("storage")
-//     if (task==null) {
-//         array=[]
-//     }else{
-//         array=JSON.parse(task)
-//     }
-//     let item=document.getElementsByClassName("items")
-//     let table= array.map((items,index)=>{
-//        return `<tr>
-//         <td>${items.id}</td>
-//         <td>${items.name}</td>
-//         <td>${items.createdDate }</td>
-//         <td>${items.completed}</td>
-//        </tr>`
-//     })   
-//     }
+function updateList() {
+    localStorage.setItem('storage', JSON.stringify(list));
+    let item = localStorage.getItem('storage');
+    // addList();
+    
+}
+
+function addList() {
+    
+    updateList()
+
+    
+    let items = document.querySelector(".items");
+    //map help us loop through obj.. item= actual obj .. index= the actual index in obj
+    let table = list.map((item, index) => {
+        return `<tr>
+        
+        <td>${index+1}</td>
+        <td>${item.name}</td>
+        <td><input type="checkbox"></td>
+        <td><button value=${index} data-delbtn >X</button></td> 
+        
+        </tr>`;
+    });
+//joins more than one row together
+    items.innerHTML = table.join('');
+}
+let getTable=document.querySelector('table')
+function deleteValue(index) {
+    list.splice(index,1)
+    // updateList()
+}
+
+getTable.addEventListener('click',function () {
+   
+    deleteValue()
+
+} )
 
 
 
