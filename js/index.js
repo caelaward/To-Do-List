e5c335d5c26e6788940204d885eb6b6fe17de82c
caelaward <146817394+caelaward@users.noreply.github.com>
2023-12-01 15:19:10 +0200

    added changes

--- js/index.js
+++ js/index.js
@@ -1,23 +1,9 @@
 let btn2=document.querySelector('[data-btn2]')
 let input=document.querySelector('[data-input]')
-b  =1 
-c = new Date();
-d = false
 let list=[]
 
-btn2.addEventListener('click', function () {
-    let maxId = 0;
-
-
-    const newItem = AddInput(input.value, maxId + 1, new Date(), false);
-    list.push(newItem);
-    updateList();
-    addList()
-})
-
-
-
-function AddInput(name, id, date, completed) {
+//function create an object from whatevr is being added
+function addInput(name, id, date, completed) {
     return {
         name: name,
         id: id,
@@ -26,6 +12,19 @@ function AddInput(name, id, date, completed) {
     };
 }
 
+//adding event listner to push the obj created into the array
+btn2.addEventListener('click', function () {
+    let maxId = 0;
+
+
+    const newItem = addInput(input.value, maxId + 1, new Date(), false);
+    list.push(newItem);
+    updateList();
+    addList()
+})
+
+
+//function to store obj in local storage and get the obj from local storage
 function updateList() {
     localStorage.setItem('storage', JSON.stringify(list));
     let item = localStorage.getItem('storage');
@@ -33,11 +32,11 @@ function updateList() {
     
 }
 
+
 function addList() {
     
     updateList()
 
-    
     let items = document.querySelector(".items");
     //map help us loop through obj.. item= actual obj .. index= the actual index in obj
     let table = list.map((item, index) => {
@@ -53,15 +52,24 @@ function addList() {
 //joins more than one row together
     items.innerHTML = table.join('');
 }
+
+
+//creating variable to select the table 
 let getTable=document.querySelector('table')
+
+//creating funtion to delete
 function deleteValue(index) {
     list.splice(index,1)
-    // updateList()
+    updateList()
 }
 
 getTable.addEventListener('click',function () {
-   
-    deleteValue()
+ if (event.target.classList.contain('data-delbtn')) {
+    table.remove()
+ }
+
+
+//    alert('Are you sure you want to delete this?')
 
 } )
 
